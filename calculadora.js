4afcb5f8b1c3687da63f2b93ca361245d9dea858
MateusTetii <mateusteti2007@gmail.com>
2023-09-01 09:23:59 -0300

    versao nova mensagem

--- calculadora.js
+++ calculadora.js
@@ -5,10 +5,11 @@ var divisao = require("./modulos/divisao");
 var subtrair = require("./modulos/subtrair");
 var mensagem = require("./modulos/mensagem");
 var nome = "Mateus";
+var msg = "Seja bem vindo";
 
 console.log("a soma de x e y é: "+ somar(45,30));
 console.log("a media de x e y é: "+ media(12,18));
 console.log("o produto de x e y é: "+ mutiplicacao(47,25));
 console.log("o quociente de x e y é: "+ divisao(100,20));
 console.log("a subtração de x e y é: "+ subtrair(87,34));
-console.log(mensagem(nome));
+console.log(mensagem(nome, msg));
